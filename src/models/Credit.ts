80dea28d06a6e6a006abc63e95b9491814bae79b
Tirawat <tirawat.pst@gmail.com>
2026-02-10 23:35:46 +0700

    update Credit table

--- src/models/Credit.ts
+++ src/models/Credit.ts
@@ -1,12 +1,25 @@
-
 import { Schema, model, models, Types } from "mongoose";
 
 const CreditSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, required: true, unique: true, index: true },
-    points: { type: Number, required: true, default: 0 },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    tiscoPoint: { type: Number, required: true, default: 0 },
+    twealthPoint: { type: Number, required: true, default: 0 },
+    tinsurePoint: { type: Number, required: true, default: 0 },
+
+    totalPoints: { type: Number, required: true, default: 0 },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
+
+CreditSchema.virtual("calculatedTotal").get(function () {
+  return this.tiscoPoint + this.twealthPoint + this.tinsurePoint;
+});
 
 export const Credit = models.Credit || model("Credit", CreditSchema);
